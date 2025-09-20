76b013256b2cb6fe10336e9198470c6e72f30e4a
Abhishek kumar verma <abhishekverma95100@gmail.com>
2025-09-20 16:01:40 +0530

    solving

---
+++ problem2.py
@@ -0,0 +1,16 @@
+num = float(input("Enter the perecentage : "))
+
+if num>=90:
+    print("Grade:A")
+elif num >=75 and num <=89:
+    print("Grade:B")
+elif num >=50 and num<=74:
+    print("Grade:C")
+elif num>=35 and num<=49:
+
+    print("Grade:D")
+
+
+else:
+
+    print("Grade:fail")
