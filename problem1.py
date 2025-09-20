24e328a94dc8e7e06157594901de62df0d81c57d
Abhishek kumar verma <abhishekverma95100@gmail.com>
2025-09-20 10:28:42 +0530

    example

--- problem1.py
+++ problem1.py
@@ -4,9 +4,9 @@ num1 = float(input("Enter the number:"))
 num2 = float(input("Enter the number:"))
 
 if num1>num2:
-    print(num1,"is greater")
+    print(num1,"is Greater")
 elif num2>num1:
-    print(num2,"is greater")
+    print(num2,"is Greater")
 else:
     print("both number are equal")
 
