229173dfae8dd8ed57086bdca82b469e74155b0e
Abhishek kumar verma <abhishekverma95100@gmail.com>
2025-09-21 10:30:23 +0530

    solving

---
+++ problem5.py
@@ -0,0 +1,14 @@
+
+color = input("Enter the traffic light(Red\Yellow\Green):")
+
+if color.lower()=="red":
+    print("Stop")
+
+elif color.lower()=="yellow":
+    print("Get Ready")
+
+elif color.lower()=="green":
+    print("Go")
+
+else:
+    print("invalid color")
