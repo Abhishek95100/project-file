5b6d681107073193fb732a233e97f3ea59fc04c8
Abhishek kumar verma <abhishekverma95100@gmail.com>
2025-09-21 10:25:38 +0530

    solving

---
+++ problem3.py
@@ -0,0 +1,9 @@
+# यूज़र से age input लो और चेक करो कि वह 18 से ऊपर है या नहीं।
+
+num = int(input("Enter the age:"))
+
+if num>=18:
+    print("you are above 18")
+
+else:
+    print("you are below 18")
